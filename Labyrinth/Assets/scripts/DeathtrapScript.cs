aa815b3f17b1972e4afd67d2b0faf1440cc63115
NachtMondVogel <68338513+NachtMondVogel@users.noreply.github.com>
2020-07-15 19:51:38 +0200

    Health System
    Added Healthsystem

---
+++ Labyrinth/Assets/scripts/DeathtrapScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DeathtrapScript : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other)
+    {
+        health = health - 1; 
+        Debug.Log(health);
+        
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
