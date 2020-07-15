bd0051386fa948455848ad440117e1315e230455
MHW <mateuszhwg@gmail.com>
2020-07-15 17:42:40 +0200

    wall 2

---
+++ Labyrinth/Assets/scripts/DeathManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DeathManager : MonoBehaviour
+{
+    [SerializeField] private Vector3 underWorld = new Vector3(0, -3, 0);
+
+    void Start()
+    {
+        InvokeRepeating("DeathChecker", 0, 1);
+    }
+
+    private void DeathChecker()
+    {
+        if (gameObject)
+        {
+            if (!(gameObject.transform.position.y < underWorld.y)) return;
+            Debug.Log("player died");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("No player detected!");
+        }
+    }
+}
---
+++ Labyrinth/Assets/scripts/PlayerChecker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerChecker : MonoBehaviour
+{
+    private void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
