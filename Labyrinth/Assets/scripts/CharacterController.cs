d1d767995f5b92fd079a001d16a750ee90df2b1f
MHW <mateuszhwg@gmail.com>
2020-07-18 11:03:30 +0200

    camera follows player
    ich habe die bewegung der camera configuriert und die Y-achse hinzugefügt.

--- Labyrinth/Assets/scripts/CharacterController.cs
+++ Labyrinth/Assets/scripts/CharacterController.cs
@@ -4,8 +4,12 @@ public class CharacterController : MonoBehaviour
 {
     [SerializeField] private float characterSpeed = 6.0f;
     [SerializeField] private float gravity = 20.0f;
-    [SerializeField] private float jumpSpeed = 8.0f;
-    [SerializeField] private float sensitivity = 50;
+    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private float sensitivityX = 50;
+    [SerializeField] private float sensitivityY = 20;
+    [SerializeField] private float clampTop = 1500;
+    [SerializeField] private float clampBottom = 20;
+    [SerializeField] private bool cameraFixed = true;
 
     private Vector3 _moveDirection = Vector3.zero;
     private float _moveIndicator;
@@ -13,14 +17,18 @@ public class CharacterController : MonoBehaviour
     //public GameObject cameraObject;
 
     private UnityEngine.CharacterController _characterController;
-    private Animator anim;
+    private Animator _anim;
+    private GameObject _camera;
 
     private float _rotationX = 0;
+    private float _rotationY = 0;
 
     void Awake()
     {
         _characterController = gameObject.GetComponent<UnityEngine.CharacterController>();
-        anim = gameObject.transform.GetChild(2).gameObject.GetComponent<Animator>();
+        _anim = gameObject.transform.GetChild(2).gameObject.GetComponent<Animator>();
+
+        if (Camera.main != null) _camera = Camera.main.gameObject;
     }
 
     private void Start()
@@ -37,39 +45,34 @@ public class CharacterController : MonoBehaviour
 
     private void PlayerRotation()
     {
-        _rotationX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime * 2.25f;
-        //float rotationY = Input.GetAxis("Mouse Y") * 100 * Time.deltaTime * (-1.25f);
+        _rotationX = Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime;
+        _rotationY = Input.GetAxis("Mouse Y") * -sensitivityY * Time.deltaTime;
 
         transform.Rotate(0, _rotationX, 0);
 
-        //tmp = Mathf.Clamp(PositionOperations.ToFloat(rotationY), -100, 100);
-        //Debug.Log("tmp: " + tmp);
-        //
-        //float rotationYFloat = rotationY;
-        //
-        //
-        // if (cameraObject.transform.rotation.eulerAngles.x >= 30.0f
-        //     && cameraObject.transform.rotation.eulerAngles.x <= 330.0f)
-        // {
-        //     if (cameraObject.transform.rotation.eulerAngles.x <= 180.0f)
-        //     {
-        //         if (rotationYFloat < 0)
-        //         {
-        //             cameraObject.transform.Rotate(rotationYFloat, 0, 0);
-        //         }
-        //     }
-        //     else
-        //     {
-        //         if (rotationYFloat > 0)
-        //         {
-        //             cameraObject.transform.Rotate(rotationYFloat, 0, 0);
-        //         }
-        //     }
-        // }
-        // else
-        // {
-        //     cameraObject.transform.Rotate(rotationYFloat, 0, 0);
-        // }
+        if (cameraFixed) return;
+        if (_camera.transform.rotation.eulerAngles.x >= clampBottom
+            && _camera.transform.rotation.eulerAngles.x <= clampTop)
+        {
+            if (_camera.transform.rotation.eulerAngles.x <= 180.0f)
+            {
+                if (_rotationY < 0)
+                {
+                    _camera.transform.Rotate(_rotationY, 0, 0);
+                }
+            }
+            else
+            {
+                if (_rotationY > 0)
+                {
+                    _camera.transform.Rotate(_rotationY, 0, 0);
+                }
+            }
+        }
+        else
+        {
+            _camera.transform.Rotate(_rotationY, 0, 0);
+        }
     }
 
     private void CharacterControllerPlayerMovement()
@@ -98,6 +101,6 @@ public class CharacterController : MonoBehaviour
 
         _moveIndicator = Mathf.Abs(_moveDirection.x) + Mathf.Abs(_moveDirection.z);
 
-        anim.SetFloat("Walk", _moveIndicator);
+        _anim.SetFloat("Walk", _moveIndicator);
     }
 }
